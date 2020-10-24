4ab782cc5b4a616fcd1ac968e31a618edc92e8ea
Linken <linken.li@cognizant.com>
2020-10-24 12:36:32 +0800

    make starter

---
+++ src/main/java/jpa/JpaGenAutoStarterConfiguration.java
@@ -0,0 +1,17 @@
+package jpa;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import jpa.autocode.controller.CodeController;
+import jpa.autocode.core.JavaCreate;
+
+
+@Configuration
+@EnableConfigurationProperties(JpaGenProperties.class)
+@Import({CodeController.class, JavaCreate.class})
+public class JpaGenAutoStarterConfiguration {
+
+	
+}
--- src/main/java/jpa/autocode/core/CreateCode.java
+++ src/main/java/jpa/autocode/core/CreateCode.java
@@ -1,5 +1,11 @@
 package jpa.autocode.core;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import jpa.autocode.bean.Parms;
+
 public interface CreateCode {
-    void create();
+    void create(EntityManager entityManager, String tableName, List<Parms> parm);
 }
